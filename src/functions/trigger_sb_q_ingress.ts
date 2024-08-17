44f8599a0819e9305e68d4d7a2fc5f1d559ca718
Kay Knöpfle <35371434+Joystick01@users.noreply.github.com>
2024-08-17 14:01:29 +0200

    eslint + prettier

--- src/functions/trigger_sb_q_ingress.ts
+++ src/functions/trigger_sb_q_ingress.ts
@@ -1,8 +1,8 @@
 import { app } from "@azure/functions";
 import { ingressHandler } from "../helpers/ingressHandler";
 
-app.serviceBusQueue('trigger_sb_q_ingress', {
-    connection: 'SERVICEBUS_CONNECTION_INGRESS',
-    queueName: process.env["INGRESS_QUEUE"],
-    handler: ingressHandler
+app.serviceBusQueue("trigger_sb_q_ingress", {
+  connection: "SERVICEBUS_CONNECTION_INGRESS",
+  queueName: process.env["INGRESS_QUEUE"],
+  handler: ingressHandler,
 });
--- src/helpers/openAiHelper.ts
+++ src/helpers/openAiHelper.ts
@@ -2,26 +2,30 @@ import { AzureOpenAI } from "openai";
 import { ChatCompletionMessageParam } from "openai/resources";
 
 export class OpenAiHelper {
+  private client: AzureOpenAI;
+  private maxTokens: number;
 
-    private client: AzureOpenAI;
-    private maxTokens: number;
+  constructor(
+    private readonly endpoint: string,
+    private readonly apiKey: string,
+    private readonly apiVersion: string,
+    private readonly deployment: string,
+    private readonly system_prompt: string,
+    maxTokens: number,
+  ) {
+    this.client = new AzureOpenAI({ endpoint, apiKey, apiVersion, deployment });
+    this.maxTokens = maxTokens;
+  }
 
-    constructor(private readonly endpoint: string, private readonly apiKey: string, private readonly apiVersion: string, private readonly deployment: string, private readonly system_prompt: string,  maxTokens: number) {
-        this.client = new AzureOpenAI({endpoint, apiKey, apiVersion, deployment});
-        this.maxTokens = maxTokens;
-    }
+  async generateResponse(
+    messages: Array<ChatCompletionMessageParam>,
+  ): Promise<string> {
+    const response = await this.client.chat.completions.create({
+      messages: [{ role: "system", content: this.system_prompt }, ...messages],
+      model: "",
+      max_tokens: this.maxTokens,
+    });
 
-    async generateResponse(messages: Array<ChatCompletionMessageParam>): Promise<string> {
-        const response = await this.client.chat.completions.create({
-            messages: [
-                {role: "system", content: this.system_prompt},
-                ...messages
-            ],
-            model: "",
-            max_tokens: this.maxTokens
-        });
-
-        return response.choices[0].message.content;
-    }
-
+    return response.choices[0].message.content;
+  }
 }
